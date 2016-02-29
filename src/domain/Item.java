3ef76b3402ccebb80a83b62f53bd11389b2c1b2b
Wen Zhang <wzhang@CNwzhang.local>
2016-02-29 16:47:29 +0800

    Initialise interview homework

---
+++ src/domain/Item.java
@@ -0,0 +1,34 @@
+package domain;
+
+/**
+ * Created by wzhang on 2/29/16.
+ */
+public class Item {
+    private double price;
+    private String name;
+    private String barcode;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+}
